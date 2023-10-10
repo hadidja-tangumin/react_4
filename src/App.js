8118867523ccf4c38d2f662be5ac870aa5a33bfa
hadidja-tangumin <141827366+hadidja-tangumin@users.noreply.github.com>
2023-10-10 18:13:46 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <input className="input" placeholder="Search"/>
+      <button className="button">Advanced Search</button>
+    </div>
+  );
+}
+
+export default App;
